58e3d32b09ec12c3b6555faf27ac801753f047a7
MatsLanGoH <mats.lan.pod@googlemail.com>
2016-07-13 22:01:22 +0900

    Solved problems 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+
+def add(num_1, num_2)
+    return num_1 + num_2
+end
+
+
+def subtract(num_1, num_2)
+    return num_1 - num_2
+end
+
+
+def sum(arr)
+    sum = 0
+    arr.each do |n|
+        sum += n        
+    end
+    return sum
+end
+
+
+def multiply(arr)
+    product = 1
+    arr.each do |n|
+        product *= n
+    end
+    return product    
+end
+
+
+def power(base, power)
+    return base ** power
+end
+
+
+def factorial(num)
+    if num == 0 or num == 1
+        return 1
+    else
+        return num * factorial(num-1)
+    end
+end
