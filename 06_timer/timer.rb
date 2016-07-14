cc6ef589feda81bc15151d86c3c9ffc0e9df2649
MatsLanGoH <mats.lan.pod@googlemail.com>
2016-07-14 23:47:31 +0900

    Solved 'padded' method of Problem 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,30 @@
 class Timer
-  #write your code here
+    #write your code here
+    attr_reader :seconds
+
+    def seconds=(seconds)
+        @seconds = seconds
+    end
+
+    def initialize (seconds=0)
+        @seconds = seconds
+    end
+
+    def time_string
+        hour_digits = "00"
+        minute_digits = "00"
+        second_digits = "00"
+
+        return hour_digits + ":" + minute_digits + ":" + second_digits
+    end
+
+
+    def padded(number)
+        if number >= 10
+            return number.to_s
+        else
+            return "0" + number.to_s
+        end
+    end
+
 end
