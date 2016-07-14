a3a967ab90392ee0aeb0a4ec7239ef39e751378c
MatsLanGoH <mats.lan.pod@googlemail.com>
2016-07-14 22:03:00 +0900

    Solved almost all problems in 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,30 @@
 #write your code here
+
+def translate_word(word)
+    if word[0] =~ /[aeiou]/
+        return word + "ay"
+    elsif word [1..3] =~ /qu/
+        return word[3..-1] + word[0..2] + "ay"
+    elsif word[0..2] =~ /qu/
+        return word[2..-1] + word[0..1] + "ay"
+    elsif word[0..2] =~ /[^aeiou]{3}/   
+        return word[3..-1] + word[0..2] + "ay"
+    elsif word[0..1] =~ /[^aeiou]{2}/
+        return word[2..-1] + word[0..1] + "ay"
+    elsif word[0] =~ /[^aeiou]/
+        return word[1..-1] + word[0] + "ay"
+    end 
+end
+
+def translate(sentence)
+    arr = []
+    sentence.split.each do |n|
+        if n[0] == n[0].upcase 
+            arr << translate_word(n.downcase).capitalize
+        else
+            arr << translate_word(n)
+        end
+    end
+    return arr.join(" ")
+end
+
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -67,6 +67,16 @@ describe "#translate" do
 
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
+  it "capitalizes words after translating" do
+    s = translate("The quick brown Fox")
+    expect(s).to eq("Ethay ickquay ownbray Oxfay")
+  end
+
   # * retain the punctuation from the original phrase
+  it "retains the punctuation from the original phrase" do
+    s = translate("The quick, brown Fox")
+    expect(s).to eq("Ethay ickquay, ownbray Oxfay")
+  end
+
 
 end
