f5b2fc069b681f32985ca5066f97bb71f3170d3f
MatsLanGoH <mats.lan.pod@googlemail.com>
2016-07-14 23:29:28 +0900

    Solved Problem 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
 # write your code here
+    attr_reader :title
+
+    def title=(name)
+        @title = capitalize_title(name)
+    end
+
+
+    def capitalize_title(book_title)
+        small_words = ["a", "an", "and", "in", "of", "the"]
+        output = []
+
+        book_title.split.each_with_index do |word, idx|
+            if (small_words.include?(word)) and (idx != 0)
+                output << word
+            else
+                output << word.capitalize 
+            end
+        end
+
+        return output.join(" ")
+    end
+
 end
